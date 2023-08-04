406493295532cae6bf8e2680da6cf602d85abb43
akirousnow <ofsanker@qq.com>
2023-08-04 19:44:36 +0800

    update

--- src/stores/useImages.ts
+++ src/stores/useImages.ts
@@ -3,7 +3,7 @@ const front = "https://xiaolai0913.oss-cn-shenzhen.aliyuncs.com/%20";
 export const getAllImageLinks = () => {
   const imageLinks = [];
   for (let i = 1; i < 28; i++) {
-    if (i === 24 || i === 11 || i === 5) continue;
+    if (i === 24 || i === 11 || i === 5||i===14) continue;
     imageLinks.push(`${front}${i}.jpg`);
   }
   return imageLinks;
